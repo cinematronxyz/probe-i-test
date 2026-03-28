90e87937fa080ff7f16a905b1cabec918c1f50e2
cinxyz <138679479+cinematronxyz@users.noreply.github.com>
2026-03-28 14:47:55 +0100

    Update skripta.sh

--- skripta.sh
+++ skripta.sh
@@ -2,17 +2,14 @@
 
 echo "Pokretanje instalacije za AR/FTN softver..."
 
-# Provera i kreiranje gtksourceview language-specs direktorijuma
 if [ ! -d /usr/share/gtksourceview-4/language-specs ]; then
     echo "Ne mogu da nadjem gtksourceview language-specs direktorijum, kreiram /usr/share/gtksourceview-4/language-specs"
     sudo mkdir -p /usr/share/gtksourceview-4/language-specs
 fi
 
-# Kopiranje asm.lang fajla
 echo "Kopiranje asm.lang u /usr/share/gtksourceview-4/language-specs..."
 sudo cp asm.lang /usr/share/gtksourceview-4/language-specs/
 
-# Postavljanje Overrides.xml
 echo "Postavljanje Overrides.xml..."
 if [ -f Overrides.xml ]; then
     echo "Postojeci Overrides.xml sacuvan kao Overrides.xml.bak"
@@ -20,21 +17,16 @@ if [ -f Overrides.xml ]; then
 fi
 sudo cp Overrides.xml /usr/share/gtksourceview-4/
 
-# Brisanje stare DDD konfiguracije
 echo "Brisanje stare DDD konfiguracije..."
 rm -rf ~/.ddd
 
-# Instalacija potrebnih paketa
 echo "Instalacija potrebnih paketa..."
 sudo apt-get update
 
-# Osnovni paketi iz prethodne skripte
 BASIC_PACKAGES="gdebi gcc diffutils grep sed ddd ddd-doc expect gcc-13-multilib gcc-multilib lib32asan8 lib32atomic1 lib32gcc-13-dev lib32gcc-s1 lib32gomp1 lib32itm1 lib32quadmath0 lib32stdc++6 lib32ubsan1 libc6-dev-x32 libc6-i386 libc6-x32 libmotif-common libssh2-1t64 libutempter0 libx32asan8 libx32atomic1 libx32gcc-13-dev libx32gcc-s1 libx32gomp1 libx32itm1 libx32quadmath0 libx32stdc++6 libx32ubsan1 libxm4 mailcap mc mc-data tcl-expect xfonts-100dpi xterm"
 
-# Dodatni razvojni paketi koje si naveo
 DEV_PACKAGES="automake bison cdbs debhelper flex info libmotif-dev libreadline-dev libtool libxaw7-dev texinfo quilt dh-autoreconf"
 
-# Instalacija svih paketa
 sudo apt-get install -y $BASIC_PACKAGES $DEV_PACKAGES
 
 echo "Sve gotovo! Pokreni 'ddd' i proveri."
